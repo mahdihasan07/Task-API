cf5c7a71805beb51e11638f0b45419c3e0ceb773
Mahdi Hasan <hasan07mahdi@gmail.com>
2026-07-05 13:51:06 +0600

    feat: add password hashing and JWT token utilities

---
+++ app/auth.py
@@ -0,0 +1,21 @@
+from datetime import datetime, timedelta
+from jose import jwt
+from passlib.contet import CryptContext
+import os
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = os.getenv("ALGORITHM")
+
+def hash_password(password: str) -> str:
+    return pwd_context.hash(password)
+
+def verify_password(plain: str, hashed: str) -> bool:
+    return pwd_context.verify(plain, hashed)
+
+def create_access_token(data: dict, expires_minutes: int = 30):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + timedelta(minutes=expires_minutes)
+    to_encode.update({"exp": expire})
+    
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
